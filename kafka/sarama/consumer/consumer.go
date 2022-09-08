2180d5adc65a1f658a560d21d75106ac65e86a28
deepsingh11421 <deepsingh11421@gmail.com>
2022-09-08 16:12:49 +0530

    Added kafka-sarama

---
+++ kafka/sarama/consumer/consumer.go
@@ -0,0 +1,92 @@
+package consumer
+
+import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/Shopify/sarama"
+	zerolog "github.com/rs/zerolog/log"
+	"github.com/wvanbergen/kafka/consumergroup"
+)
+
+var (
+	consumer    *consumergroup.ConsumerGroup
+	initialized = false
+	err         error
+)
+
+type ConsumerConfig struct {
+	AppEnv        string
+	AdminTimeout  time.Duration
+	ClientID      string
+	SASLEnable    bool
+	SASLMechanism sarama.SASLMechanism
+	SASLUser      string
+	SASLPassword  string
+	Brokers       string
+	ConsumerGroup string
+	Topic         []string
+	ZooConnection []string
+}
+
+func KafkaConsumer(config ConsumerConfig) {
+	sarama.Logger = log.New(os.Stdout, "", log.Ltime)
+
+	appEnv := config.AppEnv
+	kafkaConfig := consumergroup.NewConfig()
+	kafkaConfig.Config.ClientID = config.ClientID
+	kafkaConfig.Config.Admin.Timeout = config.AdminTimeout
+	kafkaConfig.Config.Metadata.Retry.Max = 10
+
+	if appEnv != "development" {
+		kafkaConfig.Config.Net.SASL.Enable = config.SASLEnable
+		kafkaConfig.Config.Net.SASL.Mechanism = config.SASLMechanism
+		kafkaConfig.Config.Net.SASL.User = config.SASLUser
+		kafkaConfig.Config.Net.SASL.Password = config.SASLPassword
+	}
+
+	kafkaConfig.Offsets.Initial = sarama.OffsetOldest
+	kafkaConfig.Offsets.ProcessingTimeout = 10 * time.Second
+
+	consumer, err = consumergroup.JoinConsumerGroup(config.ConsumerGroup, config.Topic, config.ZooConnection, kafkaConfig)
+	if err != nil {
+		zerolog.Fatal().Msgf(err.Error())
+	}
+	initialized = true
+	zerolog.Info().Msgf("Consumer connected")
+}
+
+func Consume(callback func(*sarama.ConsumerMessage)) {
+	if !initialized {
+		zerolog.Error().Msgf("Consumer not initialized")
+		return
+	}
+	go func() {
+		for err := range consumer.Errors() {
+			zerolog.Info().Msgf(err.Error())
+		}
+	}()
+	for {
+		select {
+		case msg := <-consumer.Messages():
+			if msg == nil {
+				return
+			}
+			callback(msg)
+
+			err := consumer.CommitUpto(msg)
+			if err != nil {
+				zerolog.Info().Msgf("Error commit zookeeper: ", err.Error())
+			}
+		}
+	}
+}
+
+func CloseConsumer() {
+	if !initialized {
+		zerolog.Error().Msgf("Consumer not initialized")
+		return
+	}
+	consumer.Close()
+}
---
+++ kafka/sarama/sync/producer.go
@@ -0,0 +1,130 @@
+package sync
+
+import (
+	"log"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/Shopify/sarama"
+	zerolog "github.com/rs/zerolog/log"
+)
+
+var syncProducer sarama.SyncProducer
+var initialized = false
+var err error
+
+type KafkaConfig struct {
+	AppEnv          string
+	ProducerTimeout time.Duration
+	AdminTimeout    time.Duration
+	ClientID        string
+	SASLEnable      bool
+	SASLMechanism   sarama.SASLMechanism
+	SASLUser        string
+	SASLPassword    string
+	Brokers         string
+}
+
+func KafkaProducer(config KafkaConfig) {
+	sarama.Logger = log.New(os.Stdout, "", log.Ltime)
+
+	kafkaConfig := sarama.NewConfig()
+
+	appEnv := config.AppEnv
+	kafkaConfig.Producer.Timeout = config.ProducerTimeout
+	kafkaConfig.ClientID = config.ClientID
+	kafkaConfig.Admin.Timeout = config.AdminTimeout
+	if appEnv != "development" {
+		kafkaConfig.Net.SASL.Enable = config.SASLEnable
+		kafkaConfig.Net.SASL.Mechanism = config.SASLMechanism
+		kafkaConfig.Net.SASL.User = config.SASLUser
+		kafkaConfig.Net.SASL.Password = config.SASLPassword
+	}
+
+	syncProducer, err = sarama.NewSyncProducer(strings.Split(config.Brokers, ","), kafkaConfig)
+	if err != nil {
+		zerolog.Fatal().Msgf(err.Error())
+	} else {
+		zerolog.Info().Msgf("Kafka connected")
+		initialized = true
+	}
+}
+
+func PushJSONMessage(m []byte, topic string) {
+	if !initialized {
+		zerolog.Error().Msgf("Kafka not initialized")
+		return
+	}
+	msg := &sarama.ProducerMessage{
+		Topic: topic,
+		Value: sarama.ByteEncoder(m),
+	}
+	p, o, err := syncProducer.SendMessage(msg)
+	if err != nil {
+		zerolog.Error().Msgf("Error publish: ", err.Error())
+	}
+	zerolog.Log().Msgf("Partition: %s ,Offset: %s", p, o)
+}
+
+func PushJSONMessages(m [][]byte, topic string) {
+	if !initialized {
+		zerolog.Error().Msgf("Kafka not initialized")
+		return
+	}
+	var msgs []*sarama.ProducerMessage
+	for _, val := range m {
+		msg := &sarama.ProducerMessage{
+			Topic: topic,
+			Value: sarama.ByteEncoder(val),
+		}
+		msgs = append(msgs, msg)
+	}
+	err := syncProducer.SendMessages(msgs)
+	if err != nil {
+		zerolog.Error().Msgf("Error publish: ", err.Error())
+	}
+}
+
+func PushStringMessage(m string, topic string) {
+	if !initialized {
+		zerolog.Error().Msgf("Kafka not initialized")
+		return
+	}
+	msg := &sarama.ProducerMessage{
+		Topic: topic,
+		Value: sarama.StringEncoder(m),
+	}
+	p, o, err := syncProducer.SendMessage(msg)
+	if err != nil {
+		zerolog.Error().Msgf("Error publish: ", err.Error())
+	}
+	zerolog.Log().Msgf("Partition: %s ,Offset: %s", p, o)
+}
+
+func PushStringMessages(m []string, topic string) {
+	if !initialized {
+		zerolog.Error().Msgf("Kafka not initialized")
+		return
+	}
+	var msgs []*sarama.ProducerMessage
+	for _, val := range m {
+		msg := &sarama.ProducerMessage{
+			Topic: topic,
+			Value: sarama.StringEncoder(val),
+		}
+		msgs = append(msgs, msg)
+	}
+	err := syncProducer.SendMessages(msgs)
+	if err != nil {
+		zerolog.Error().Msgf("Error publish: ", err.Error())
+	}
+}
+
+func CloseProducer() {
+	if !initialized {
+		zerolog.Error().Msgf("Kafka not initialized")
+		return
+	}
+	syncProducer.Close()
+}
